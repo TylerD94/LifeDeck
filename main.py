f4c55a682a30d9d983e6b591a436708e214b5ecd
TylerD94 <59483267+TylerD94@users.noreply.github.com>
2021-10-31 18:50:48 -0400

    Add files via upload
    Added all buttons for full up and down life support. 
    Added game over functionality
    Still need to come up with a way to change starting life total between 20 and 40.

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ p2_hp = 40
 lcds = []
 
 # Empty array to hold buttons
-buttons = [] # TODO create set_up_buttons, physically create 3 more buttons
+buttons = []
 
-p1_up_btn = Pin(15, Pin.IN, Pin.PULL_UP)
+
 
 def set_up_lcds(lcds): # Find and set LCD display addresses    
     i2c_1 = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
@@ -29,10 +29,21 @@ def set_up_lcds(lcds): # Find and set LCD display addresses
     lcd_1 = I2cLcd(i2c_1, I2C_ADDR_1, 2, 16)
     lcd_2 = I2cLcd(i2c_2, I2C_ADDR_2, 2, 16) 
     
-    lcds.append(lcd_1)
-    lcds.append(lcd_2)
+    lcds.extend((lcd_1, lcd_2))
     
     return lcds
+
+
+def set_up_buttons(buttons):
+    p1_up_btn = Pin(12, Pin.IN, Pin.PULL_UP)
+    p1_down_btn = Pin(13, Pin.IN, Pin.PULL_UP)
+    p2_up_btn = Pin(14, Pin.IN, Pin.PULL_UP)
+    p2_down_btn = Pin(15, Pin.IN, Pin.PULL_UP)
+    
+    buttons.extend((p1_up_btn, p1_down_btn, p2_up_btn, p2_down_btn))
+    
+    return buttons
+
 
 def display_version():
     for lcd in lcds:
@@ -43,40 +54,69 @@ def display_version():
     for lcd in lcds:
         lcd.clear()
 
-def init_display_hp(p1_hp):
+
+def init_display_hp(p1_hp, p2_hp):
     for lcd in lcds:
-        lcd.putstr(f"P1: {p1_hp}")
+        lcd.clear()
+        lcd.putstr(f"P1: {p1_hp}  P2: {p2_hp}")
 
-def display_hp(lcd, p1_hp):
-    lcd.putstr(f"P1: {p1_hp}")
 
-def change_hp():
-    global p1_hp
-    if(p1_up_btn.value()) == 0:
-        p1_hp -= 1
-        for lcd in lcds:
-            lcd.clear()
-        for lcd in lcds:
-            display_hp(lcd, p1_hp)
-        time.sleep(0.5)
+def display_hp(lcd, p1_hp, p2_hp):
+    lcd.putstr(f"P1: {p1_hp}  P2: {p2_hp}")
+
+
+def update_hp_display(p1_hp, p2_hp):  
+    for lcd in lcds:
+        lcd.clear()
+    for lcd in lcds:
+        display_hp(lcd, p1_hp, p2_hp)
+    time.sleep(0.5)
         
-def game_over(p1_hp):
-    if(p1_hp == 0):
+        
+
+def change_hp(buttons):
+    global p1_hp, p2_hp
+    
+    if(buttons[0].value() == 0):
+        p1_hp += 1
+        update_hp_display(p1_hp, p2_hp)
+    elif(buttons[1].value() == 0):
+        p1_hp -= 1
+        update_hp_display(p1_hp, p2_hp)
+    elif(buttons[2].value() == 0):
+        p2_hp += 1
+        update_hp_display(p1_hp, p2_hp)
+    elif(buttons[3].value() ==0):
+        p2_hp -= 1
+        update_hp_display(p1_hp, p2_hp)
+
+
+def game_over(lcds, p1_hp, p2_hp):
+    if(p1_hp == 0 or p2_hp == 0):
         for lcd in lcds:
             lcd.clear()
             lcd.putstr("GAME OVER")
-        time.sleep(1)
-        exit()
-        #lcd.clear()
+        time.sleep(0.5)
+        for lcd in lcds:
+            lcd.move_to(0,1)
+        if(p1_hp == 0):
+            lcds[0].putstr("YOU LOSE")
+            lcds[1].putstr("YOU WIN")
+        elif(p2_hp == 0):
+            lcds[0].putstr("YOU WIN")
+            lcds[1].putstr("YOU LOSE")
+        time.sleep(3)
+        exit() # Not a proper stop, but stops the program anyway.
+
 
 
 set_up_lcds(lcds)
+set_up_buttons(buttons)
 display_version()
-init_display_hp(p1_hp)
+init_display_hp(p1_hp, p2_hp)
 
 
 while True:
-    change_hp()
-    game_over(p1_hp)
-    
-
+    change_hp(buttons)
+    game_over(lcds, p1_hp, p2_hp)
+ 
